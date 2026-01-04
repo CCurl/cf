7ffe6409d113690eba8aa2169be1d59fb8908d14
Chris Curl <curlc@yahoo.com>
2026-01-04 10:53:31 -0500

    wtf

--- system.c
+++ system.c
@@ -3,58 +3,58 @@
 #include "cf.h"
 
 #ifdef IS_WINDOWS
-#include <windows.h>
-#include <conio.h>
-int qKey() { return _kbhit(); }
-int key() { return _getch(); }
-void ttyMode(int isRaw) {}
-void ms(cell sleepForMS) { Sleep((DWORD)sleepForMS); }
+	#include <windows.h>
+	#include <conio.h>
+	int qKey() { return _kbhit(); }
+	int key() { return _getch(); }
+	void ttyMode(int isRaw) {}
+	void ms(cell sleepForMS) { Sleep((DWORD)sleepForMS); }
 #endif
 
 // Support for Linux, OpenBSD, FreeBSD
 #if defined(__linux__) || defined(__OpenBSD__) || defined(__FreeBSD__)
-#include <termios.h>
-#include <unistd.h>
-#include <sys/time.h>
+	#include <termios.h>
+	#include <unistd.h>
+	#include <sys/time.h>
 
-void ttyMode(int isRaw) {
-	static struct termios origt, rawt;
-	static int curMode = -1;
-	if (curMode == -1) {
-		curMode = 0;
-		tcgetattr( STDIN_FILENO, &origt);
-		cfmakeraw(&rawt);
-	}
-	if (isRaw != curMode) {
-		if (isRaw) {
-			tcsetattr( STDIN_FILENO, TCSANOW, &rawt);
-		} else {
-			tcsetattr( STDIN_FILENO, TCSANOW, &origt);
+	void ttyMode(int isRaw) {
+		static struct termios origt, rawt;
+		static int curMode = -1;
+		if (curMode == -1) {
+			curMode = 0;
+			tcgetattr( STDIN_FILENO, &origt);
+			cfmakeraw(&rawt);
 		}
-		curMode = isRaw;
+		if (isRaw != curMode) {
+			if (isRaw) {
+				tcsetattr( STDIN_FILENO, TCSANOW, &rawt);
+			} else {
+				tcsetattr( STDIN_FILENO, TCSANOW, &origt);
+			}
+			curMode = isRaw;
+		}
 	}
-}
-int qKey() {
-	struct timeval tv;
-	fd_set rdfs;
-	ttyMode(1);
-	tv.tv_sec = 0;
-	tv.tv_usec = 0;
-	FD_ZERO(&rdfs);
-	FD_SET(STDIN_FILENO, &rdfs);
-	select(STDIN_FILENO+1, &rdfs, NULL, NULL, &tv);
-	int x = FD_ISSET(STDIN_FILENO, &rdfs);
-	// ttyMode(0);
-	return x;
-}
-int key() {
-	ttyMode(1);
-	int x = fgetc(stdin);
-	// ttyMode(0);
-	return x;
-}
-void ms(cell sleepForMS) {
-	if (sleepForMS > 0) { usleep(sleepForMS * 1000); }
+	int qKey() {
+		struct timeval tv;
+		fd_set rdfs;
+		ttyMode(1);
+		tv.tv_sec = 0;
+		tv.tv_usec = 0;
+		FD_ZERO(&rdfs);
+		FD_SET(STDIN_FILENO, &rdfs);
+		select(STDIN_FILENO+1, &rdfs, NULL, NULL, &tv);
+		int x = FD_ISSET(STDIN_FILENO, &rdfs);
+		// ttyMode(0);
+		return x;
+	}
+	int key() {
+		ttyMode(1);
+		int x = fgetc(stdin);
+		// ttyMode(0);
+		return x;
+	}
+	void ms(cell sleepForMS) {
+		if (sleepForMS > 0) { usleep(sleepForMS * 1000); }
 	}
 #endif // Linux, OpenBSD, FreeBSD
 
